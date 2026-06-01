1478563ac3e8a0d29a9c808aa089a61dd428e447
Shravani <bhavinidawane@gmail.com>
2026-06-01 13:05:44 +0530

    Done

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 const navbar = document.getElementById("navbar");
+const navLinks = document.querySelectorAll(".nav-links a");
 
-window.addEventListener("scroll", function() {
-
-    if(window.scrollY > 50){
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 100) {
         navbar.classList.add("scrolled");
-    }else{
+    } else {
         navbar.classList.remove("scrolled");
     }
+});
 
+navLinks.forEach((link) => {
+    link.addEventListener("mouseenter", () => {
+        link.classList.add("hovered");
+    });
+    link.addEventListener("mouseleave", () => {
+        link.classList.remove("hovered");
+    });
 });
